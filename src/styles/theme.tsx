77ce59fd4c7dc091430b6faf85ef4029c712d83e
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2023-07-02 14:26:35 -0300

    feat: criando o theme

---
+++ src/styles/theme.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#E6E6FA",
+      main: "#191970",
+      dark: "#002100",
+    },
+    grey: {
+      100: "#F8F8F8",
+      600: "#666666",
+    },
+  },
+});
+
+export default theme;
