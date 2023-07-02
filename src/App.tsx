5d1db0c26c373f1efa47ed64f7caca9d423afac9
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2023-07-02 20:28:19 -0300

    feat: adicionando provider do contexto do usuario

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { CssBaseline, ThemeProvider } from "@mui/material"
 import theme from "./styles/theme"
 import { BrowserRouter } from "react-router-dom"
 import Router from "./routes"
+import { UserProvider } from "./context/userContext"
 
 function App() {
 
   return (
     <>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <CssBaseline />
-        <Router />
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <CssBaseline />
+          <Router />
+        </BrowserRouter>
+      </UserProvider>
     </ThemeProvider>
     </>
   )
