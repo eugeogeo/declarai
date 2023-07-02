77b44c607a3b7d1d1d7ce9fde00a763813e397ed
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2023-07-02 16:19:41 -0300

    feat: criando base do cadastro de novo usuario

---
+++ src/pages/NovoUsuario/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const NovoUsuario = () => {
+
+  return(
+    <>
+      <Typography>oi em cadastro de academico</Typography>
+    </>
+  );
+}
+export default NovoUsuario;
